c8786281d0e2ef706c22f7a2d06c72fa4ce2f6cd
nguyenthang338 <nguyenthang33894@gmail.com>
2019-06-14 09:03:27 +0700

    branch push remote

---
+++ git/branch-push-remote.sh
@@ -0,0 +1,2 @@
+#If you are publishing a local branch for the first time on a remote, the "-u" option is helpful. It makes sure that a tracking connection between the local and the newly created remote branch is established
+git push -u origin develop
